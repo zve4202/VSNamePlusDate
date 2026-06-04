fde9dfdeb14c611c092b356e1a35702cd7561248
Admin <Admin@GHOST>
2026-06-04 05:50:24 +0300

    VSNamePlusDate 2026-06-04 05:50:03

--- Commands/NamePlusDateCommand.cs
+++ Commands/NamePlusDateCommand.cs
@@ -15,8 +15,6 @@ namespace VSNamePlusDate
     [Command(PackageIds.NamePlusDateCommand)]
     internal sealed class NamePlusDateCommand : BaseCommand<NamePlusDateCommand>
     {
-
-        
         protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
         {
             await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
