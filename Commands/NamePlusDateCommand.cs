0bba7bc55e6e6a1d07feee2ec2f0230663b602ab
Admin <Admin@GHOST>
2026-06-04 04:14:30 +0300

    Добавьте файлы проекта.

---
+++ Commands/NamePlusDateCommand.cs
@@ -0,0 +1,45 @@
+﻿using EnvDTE;
+using Microsoft.VisualStudio.Text;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VSNamePlusDate
+{
+    [Command(PackageIds.NamePlusDateCommand)]
+    internal sealed class NamePlusDateCommand : BaseCommand<NamePlusDateCommand>
+    {
+        protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
+        {
+            DocumentView? docView = await VS.Documents.GetActiveDocumentViewAsync();
+
+            SnapshotSpan? selection = docView?.TextView.Selection.SelectedSpans.FirstOrDefault();            
+            
+            if (docView == null || selection == null)
+                return;
+
+            string name = await GetSolutionNameAsync();
+
+            if (string.IsNullOrWhiteSpace(name))
+                name = "Project";
+
+            string stamp = $"{name} {DateTime.Now:yyyy-MM-dd HH:mm:ss}";
+
+            docView.TextBuffer.Replace(selection.Value, stamp);
+        }
+
+        private async Task<string> GetSolutionNameAsync()
+        {
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+
+            DTE? dte = await VS.GetServiceAsync<DTE, DTE>();
+
+            string? solutionPath = dte?.Solution?.FullName;
+
+            if (string.IsNullOrWhiteSpace(solutionPath))
+                return string.Empty;
+
+            return Path.GetFileNameWithoutExtension(solutionPath);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# VSMamePlusDate
